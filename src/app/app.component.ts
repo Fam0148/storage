3e77067caffb737f7ed690cd035cfd7b577aaae9
sibbe23 <subashssubramaniank@gmail.com>
2024-02-12 15:47:19 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'storage';
+export class AppComponent 
+{
+
+  appObj:StorageList;
+  taskList:StorageList[]=[];
+
+  constructor()
+  {
+    this.appObj= new StorageList()
+  }
+
+  submitList()
+  {
+    const obj = JSON.stringify(this.appObj)
+    const pObj = JSON.parse(obj)
+    this.taskList.push(pObj)
+    localStorage.setItem("database", JSON.stringify(this.taskList))
+  }
+}
+
+
+export class StorageList 
+{
+data: string;
+date: string;
+tags: string;
+
+constructor()
+{
+  this.data='';
+  this.date='';
+  this.tags='';
+}
 }
